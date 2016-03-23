9fd85dce8d841629befa691d62b52eb62f351316
Michael Barbier <michael.barbier@uantwerpen.be>
2016-03-23 13:17:50 +0100

    First tests to include pre-registration error calculation

--- src/be/ua/mbarbier/rese/ReSe_Demo.java
+++ src/be/ua/mbarbier/rese/ReSe_Demo.java
@@ -2,11 +2,14 @@ package be.ua.mbarbier.rese;
 
 import java.io.File;
 
+import be.ua.mbarbier.rese.image.LibUtilities;
 import ij.IJ;
 import ij.ImageJ;
 import ij.plugin.*;
+import ij.plugin.filter.Binary;
+import ij.process.ImageProcessor;
+import ij.process.ImageStatistics;
 import ij.ImagePlus;
-
 
 /**
  * @author mbarbie1
@@ -18,7 +21,7 @@ public class ReSe_Demo implements PlugIn {
 	ImagePlus target;
 
 	public void scenario_showImage(String arg) {
-		
+
 		File srcFile = new File("c:/Users/Michael/Desktop/demo/input/20.png");
 		File refFile = new File("c:/Users/Michael/Desktop/demo/input/31.png");
 		this.source = IJ.openImage( srcFile.getAbsolutePath() );		
@@ -27,8 +30,46 @@ public class ReSe_Demo implements PlugIn {
 		this.target.show();
 	}
 
-	public void scenario_PreRegistration(String arg) {
+	public ImagePlus scaleImp( ImagePlus imp, int binning) {
+		int newWidth = imp.getWidth() / binning;
+		ImageProcessor ip = imp.getProcessor();
+		ip.setInterpolationMethod(ip.BILINEAR);
+		ImageProcessor scaledSource = ip.resize( newWidth );
+		ImagePlus scaledImp = new ImagePlus("scaled", scaledSource);
 		
+		return scaledImp;
+	}
+
+	public ImagePlus smoothMask( ImagePlus imp, int binning) {
+		
+		ImageProcessor ip = imp.getProcessor();
+		ImagePlus scaledImp = scaleImp( imp, 4);
+		ImageProcessor scaledIp = scaledImp.getProcessor();
+		//if ( scaledIp.getBitDepth() > 8 ) {
+		//	scaledIp.con
+		//} 
+		ImageStatistics stats = scaledImp.getStatistics();
+		
+		
+		long[] histo = stats.getHistogram();
+
+		//scaledIp.setAutoThreshold( , );
+		scaledIp.autoThreshold();
+	 	new ImagePlus( "mask", scaledIp ).show();
+	 	ip.autoThreshold();
+	 	new ImagePlus( "mask", ip ).show();
+
+		ImagePlus maskImp = LibUtilities.mask( scaledIp, 0, 128 );
+
+		IJ.run(scaledImp, "Histogram", "");
+		IJ.run(imp, "Histogram", "");
+
+
+		return maskImp;
+	}
+
+	public void scenario_PreRegistration(String arg) {
+
 		//File srcFile = new File("c:/Users/Michael/Desktop/demo/input/20.png");
 		File srcFile = new File("c:/Users/Michael/Desktop/demo/input/7.png");
 		File refFile = new File("c:/Users/Michael/Desktop/demo/input/31.png");
@@ -36,17 +77,17 @@ public class ReSe_Demo implements PlugIn {
 		this.target = IJ.openImage( refFile.getAbsolutePath() );		
 		this.source.show();
 		this.target.show();
-		
-		
-		
+		ImagePlus scaledImp = scaleImp( this.source, 4);
+		scaledImp.show();
+		smoothMask( this.source, 4 );
+
 	}
-	
+
 	@Override
 	public void run(String arg) {
 
-		scenario_showImage(arg);
+		//scenario_showImage(arg);
 		scenario_PreRegistration(arg);
-		
 	}
 
 	public static void main(String[] args) {
--- src/be/ua/mbarbier/rese/image/LibUtilities.java
+++ src/be/ua/mbarbier/rese/image/LibUtilities.java
@@ -81,6 +81,7 @@ public class LibUtilities {
 		return ip1Temp;
 	}
 
+	
 }
 
 
